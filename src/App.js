a6fb6b8a1148e70b76f9dd61bc22b37c8cd5e90c
Shashi Ji <shashikumarkushwaha3@gmail.com>
2023-08-04 13:35:06 +0530

    quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getNewQuote = async () => {
+    try {
+      const response = await axios.get("https://api.quotable.io/random");
+      const data = response.data;
+      console.log(response);
+      console.log(data);
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  useEffect(() => {
+    getNewQuote();
+  }, []);
+
+  const tweetQuote = () => {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      `"${quote}" - ${author}`
+    )}`;
+    window.open(tweetUrl, "_blank");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className=" center">
+      <div id="quote-box" className="q">
+        <div id="text">{quote}</div>
+        <div id="author">- {author}</div>
+        <button id="new-quote" className="ms-1" onClick={getNewQuote}>
+          New Quote
+        </button>
+        <a id="tweet-quote" className="btn" href="#" onClick={tweetQuote}>
+          Tweet Quote
         </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
